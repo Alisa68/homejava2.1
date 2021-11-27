81ea41e83be67cbe3284b2d2551d1486d6a2fb4d
Alisa68 <lebedeva19682@yandex.ru>
2021-11-27 09:13:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+           int ticket = 3456;
+           int onemile = 20;
+
+           int bonusmile = ticket / onemile;
+
+          System.out.println(bonusmile);
+    }
+}
